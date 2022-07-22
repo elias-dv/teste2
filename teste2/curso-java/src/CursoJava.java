d7b609abd2dbb0298ffc7d4dec58e4e229f68c9a
elias <elias@air-de-elias>
2022-07-22 11:37:55 -0300

    aaa

--- teste2/curso-java/src/CursoJava.java
+++ teste2/curso-java/src/CursoJava.java
@@ -3,7 +3,7 @@ class CursoJava {
     public static void main(String[] args) {
    
        System.out.println("Primeiro progama em Java");
-
+       System.out.println("Hello Java");     
    }
 
    }
